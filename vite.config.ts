8d6f3f0bd6794c53b09cbaa4ca58f8cee889c223
SathvikMunaga <mnsathvik123@gmail.com>
2025-12-16 16:43:19 +0530

    Update Vite config with base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,10 @@
 import path from 'path';
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/your-repo-name/',
+})
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
